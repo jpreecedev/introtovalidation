ba0b4b74f5f6d688f56d84c8022788274da1ef06
jpreecedev <jonpreece@hotmail.co.uk>
2014-03-15 20:04:07 +0000

    First commit - Created the basic code and added some simple validation

---
+++ IntroToValidation/_references.js
@@ -0,0 +1,5 @@
+﻿/// <reference path="index.js" />
+/// <reference path="Scripts/bootstrap.js" />
+/// <reference path="Scripts/jquery-1.9.0.intellisense.js" />
+/// <reference path="Scripts/jquery.validate-vsdoc.js" />
+/// <reference path="Scripts/jquery.validate.js" />
